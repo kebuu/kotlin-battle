6ea57745ffa21b96eb7917b4a15603904b786730
Christophe TARDELLA <christophe.tardella@gmail.com>
2016-12-09 01:07:54 +0100

    Correction bug

--- kotlin-battle-core/src/main/kotlin/com/kebuu/core/dto/board/item/TreasureBoardItemDto.kt
+++ kotlin-battle-core/src/main/kotlin/com/kebuu/core/dto/board/item/TreasureBoardItemDto.kt
@@ -2,5 +2,5 @@ package com.kebuu.core.dto.board.item
 
 import com.kebuu.core.Position
 
-class TreasureBoardItemDto(position: Position = Position.ORIGIN, val zPoints: Int = 0): AbstractBoardItemDto(position)
+class TreasureBoardItemDto(position: Position = Position.ORIGIN, var zPoints: Int = 0): AbstractBoardItemDto(position)
 
